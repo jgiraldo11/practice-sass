97f327d90a978c852b90c079a7f6b4cc22aba613
Jeniffer Giraldo <jgiraldo.dev@gmail.com>
2023-07-25 16:31:41 -0400

    css animation scss

---
+++ src/components/BoxRace/BoxRace.jsx
@@ -0,0 +1,15 @@
+export default function BoxRace() {
+
+  return (
+    <>
+    <h2>Box Race: Timing Function</h2>
+      <div className="boxrace-container">
+        <div className="boxrace race1">Linear</div>
+        <div className="boxrace race-ease">Ease</div>
+        <div className="boxrace race-ease-in">Ease In</div>
+        <div className="boxrace race-ease-out">Ease Out</div>
+        <div className="boxrace race-ease-in-out">Ease In Out</div>
+      </div>
+    </>
+  );
+};
